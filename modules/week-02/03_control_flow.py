d3e1af1e948d7e147a6efd81f4e553bafca8b9e9
Jeff Ryan <jefrya123@gmail.com>
2025-05-19 13:25:59 -0500

    Added Control Flow Practice

---
+++ modules/week-02/03_control_flow.py
@@ -0,0 +1,19 @@
+# if 
+age = int(input("Whats your age? "))
+if age >= 30:
+    print("Your over the hill")
+else:
+    print("Your still climbing") 
+
+# for looping
+
+games=["Zelda", "Mario", "Pokemon"]
+for game in games:
+    print(game)
+
+#while looping
+counter = 1
+
+while counter <= 5:
+    print("Counter is at", counter)
+    counter += 1
