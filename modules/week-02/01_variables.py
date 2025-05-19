93b23e13bc90cb04f81e3088d8556026aad9938a
Jeff Ryan <jefrya123@gmail.com>
2025-05-19 11:57:59 -0500

    Added 01 Variables Practice

---
+++ modules/week-02/01_variables.py
@@ -0,0 +1,15 @@
+# variables
+
+name= "Jeff"
+age = 33
+height = 5.10
+student = True
+print(type(name), type(age), type(height), type(student))
+
+# Mathing
+print(44 + 33)
+print(15 - 7)
+print(80 * 400)
+print(1000 / 25)
+print(16 % 7)
+print(4 ** 8)
